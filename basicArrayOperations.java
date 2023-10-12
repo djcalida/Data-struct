2da41ffb2a417414d943ebc12568206cb6159a56
unknown <djcalida@gmail.com>
2023-10-12 21:09:24 +0800

    Kaya pani

--- basicArrayOperations.java
+++ basicArrayOperations.java
@@ -1,13 +1,23 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class basicArrayOperations {
+public class BasicArrayOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> myArray = new ArrayList<>();
 
+        System.out.println("Enter 5 integers: ");
+        
+        //Scan the first 5 integers inputs 
+        for (int i = 0; i < 5; i++) {
+            int input = scanner.nextInt();
+            myArray.add(input);
+        }
+        //Dislplay the first 5 integers entered
+        System.out.println("You entered the following integers: " + myArray);
+
         while (true) {
-            System.out.println("\nChoose an element:");
+            System.out.println("\nChoose an operation:");
             System.out.println("1. Insert an element at the beginning");
             System.out.println("2. Insert an element at the end");
             System.out.println("3. Delete an element from a specific position");
@@ -16,9 +26,9 @@ public class basicArrayOperations {
             System.out.println("6. Print the array");
             System.out.println("7. Exit");
 
-            System.out.print("/nEnter your choice: ");
+            System.out.print("\nEnter your choice: ");
             int choice = scanner.nextInt();
-
+            
             switch (choice) {
                 case 1:
                     System.out.print("Enter the element to insert at the beginning: ");
@@ -36,7 +46,7 @@ public class basicArrayOperations {
                     if (positionToDelete >= 0 && positionToDelete < myArray.size()) {
                         myArray.remove(positionToDelete);
                     } else {
-                        System.out.println("\nSobra rasad.");
+                        System.out.println("\nInvalid position.");
                     }
                     break;
                 case 4:
@@ -61,14 +71,14 @@ public class basicArrayOperations {
                     }
                     break;
                 case 6:
-                    System.out.println("\nArrays: " + myArray);
+                    System.out.println("\nArray: " + myArray);
                     break;
                 case 7:
-                    System.out.println("Humana");
+                    System.out.println("Exiting");
                     scanner.close();
                     System.exit(0);
                 default:
-                    System.out.println("\n\nPick a number from 1-7.");
+                    System.out.println("\nPick a number from 1-7.");
             }
         }
     }
